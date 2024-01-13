6919b300fc6a0d46d7203ae5002c5b95b53b053b
Dtsiantaris <44876922+Dtsiantaris@users.noreply.github.com>
2024-01-13 20:27:50 +0200

    Create CustomStepperButton.vue

---
+++ CustomStepperButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-btn
+    color="secondary"
+    unelevated
+    flat
+    padding="0"
+    :icon="type === 'up' ? 'arrow_drop_up' : 'arrow_drop_down'"
+    dense
+    class="w-[2.8rem] "
+  />
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+defineProps({
+  type: {
+    type: String as PropType<'up' | 'down'>,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
